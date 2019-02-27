71f9a91dbaac9312cea612b4e698e952c9dabd52
Jack Gay <shaungay@apples-mbp-2.home>
2019-02-27 12:30:08 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,8 +20,11 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
 var cardsInPlay = [];
+
 var checkForMatch = function() {
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 alert("You found a match!");
 } else {
@@ -29,13 +32,20 @@ alert("Sorry, try again.");
 }
 };
 
-var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.querySelector("#game-board").appendChild(cardElement);
+}
+};
+
+createBoard();
